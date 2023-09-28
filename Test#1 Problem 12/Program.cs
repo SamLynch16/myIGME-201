cc261a545bcf44218ec15320afb70f38d149b6a5
Sam Lynch <s2003lynch@gmail.com>
2023-09-28 19:01:32 -0400

    problem 12 test 1

---
+++ Test#1 Problem 12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1_Problem_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+
+            Console.WriteLine("What is your name?");
+            sName = Console.ReadLine();
+            GiveRaise(sName, ref dSalary);
+            Console.WriteLine($"Congratulations, {sName} you got a raise! New Salary:{dSalary}");
+        }
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if (name == name)
+            {
+                salary += 19999.99;
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
