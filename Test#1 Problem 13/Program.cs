9daf66408c27d7c498253d28fdd631399a21a449
Sam Lynch <s2003lynch@gmail.com>
2023-09-29 17:29:59 -0400

    problem 13

---
+++ Test#1 Problem 13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Test_1_Problem_13
+{
+    internal class Program
+    {
+        struct Employee
+        {
+            public string sName;
+            public double dSalary;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Employee emp = new Employee();
+            emp.sName = name;
+            emp.dSalary = 30000;
+            bool newsalary = GiveRaise(ref emp);
+
+            Console.WriteLine($"Congratulations, {emp.sName} you got a raise! New Salary:{emp.dSalary}");
+        }
+        static bool GiveRaise(ref Employee emp)
+        {
+            if (emp.sName == emp.sName)
+            {
+                emp.dSalary += 19999.99;
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
+        
+       
+
+    
+
+
