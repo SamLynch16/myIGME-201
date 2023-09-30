5ecfd874b945209aae10b7b7bcd21a6c5ba660d6
Sam Lynch <s2003lynch@gmail.com>
2023-09-30 16:42:42 -0400

    test 1 problem 3

---
+++ Test#1 Problem 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1_Problem_3
+{
+    internal class Program
+    {
+        delegate int MathRound(double numberdeci);
+
+        static int nRound(double nNumber){
+            return (int)Math.Round(nNumber);
+
+}
+        static void Main(string[] args)
+        {
+            string sNumber = null;
+            double nNum = 0;
+       
+            do
+            {
+                Console.Write("Enter a decimal: ");
+                sNumber = Console.ReadLine();
+            } while (!double.TryParse(sNumber, out nNum));
+
+            MathRound rDelegate = nRound;
+
+            int rounded = rDelegate(nNum);
+            Console.WriteLine(rounded);
+
+            // prompt and convert second number
+            
+        }
+    }
+}
