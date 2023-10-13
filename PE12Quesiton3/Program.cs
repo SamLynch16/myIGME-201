a50710ad94ff39548e9dc611ce640004b023cc4e
Sam Lynch <s2003lynch@gmail.com>
2023-10-13 16:55:32 -0400

    MyClass and MyDerivedClass

---
+++ PE12Quesiton3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass dclassobject = new MyDerivedClass("iValue");
+            string result = dclassobject.GetString();
+
+            Console.WriteLine(result);
+        }
+
+        public class MyClass {
+            private string myString;
+
+            public MyClass(string iValue) {
+                myString = iValue;
+            }
+            
+            public virtual string GetString()
+            {
+                return myString;
+            }
+        }
+
+        public class MyDerivedClass : MyClass
+        {
+            public MyDerivedClass(string iValue) : base(iValue)
+            {
+
+            }
+
+            public override string GetString() {
+                string baseString = base.GetString();
+                return baseString + "(output from the derived class)";
+            }
+        }
+    }
+}
