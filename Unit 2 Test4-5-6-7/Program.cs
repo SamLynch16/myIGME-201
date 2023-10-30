4215777d18d7ea51e9eb6710b87dca3d9dee227e
Sam Lynch <s2003lynch@gmail.com>
2023-10-29 20:14:18 -0400

    unit 2 test

---
+++ Unit 2 Test4-5-6-7/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_2_Test4_5_6_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tardis tardis = new Tardis
+            {
+                whichDrWho = 10
+            };
+
+            PhoneBooth pBooth = new PhoneBooth
+            {
+                costPerCall = 5.0
+            };
+
+            UsePhone(tardis);
+            UsePhone(pBooth);
+        }
+        
+
+        static void UsePhone(object obj)
+        {
+            if (obj is IPhoneInterface phone)
+            {
+                phone.MakeCall();
+                phone.HangUp();
+            }
+           
+            if (obj is PhoneBooth pbooth)
+            {
+                pbooth.MakeCall();
+            }
+            else if (obj is Tardis tard)
+            {
+                tard.TimeTravel();
+            }
+        }
+
+        public interface IPhoneInterface
+        {
+            void Answer();
+            void MakeCall();
+            void HangUp();
+
+        }
+        public abstract class Phone
+        {
+            private string phoneNumber;
+            public string address;
+
+            public string PhoneNumber;
+            public abstract void Connect();
+            public abstract void Disconnect();
+            
+        }
+        public class RotaryPhone: Phone, IPhoneInterface
+        {
+            public void Answer()
+            {
+
+            }
+            public void MakeCall()
+            {
+
+            }
+            public void HangUp()
+            {
+
+            }
+            public override void Connect()
+            {
+
+            }
+            public override void Disconnect()
+            {
+                
+            }
+        }
+        public class PushButtonPhone : Phone, IPhoneInterface
+        {
+            public void Answer()
+            {
+
+            }
+            public void MakeCall()
+            {
+
+            }
+            public void HangUp()
+            {
+
+            }
+            public override void Connect()
+            {
+
+            }
+            public override void Disconnect()
+            {
+
+            }
+        }
+
+        public class PhoneBooth : PushButtonPhone
+        {
+            private bool superMan;
+            public double costPerCall;
+            public bool phoneBook;
+
+            public void OpenDoor()
+            {
+
+            }
+            public void CloseDoor()
+            {
+
+            }
+        }
+        public class Tardis : PushButtonPhone
+        {
+            private bool sonicScrewdriver;
+            
+            private string femaleSideKick;
+            public double exteriorSurfaceArea;
+            public double interiorVolume;
+
+            public byte WhichDrWho { get;}
+            public string FemaleSideKick { get;}
+
+            public void TimeTravel()
+            {
+
+            }
+            public byte whichDrWho { get; set; }
+
+            public static bool operator ==(Tardis t1, Tardis t2)
+            {
+                if (t1 is null && t2 is null)
+                {
+                    return true;
+                }
+                if (t1 is null || t2 is null)
+                {
+                    return false;
+                }
+                if (t1.whichDrWho == 10)
+                {
+                    return true;
+                }
+                if (t2.whichDrWho == 10)
+                {
+                    return false;
+                }
+                return t1.whichDrWho == t2.whichDrWho;
+            }
+
+            public static bool operator !=(Tardis t1, Tardis t2)
+            {
+                return !(t1 == t2);
+            }
+
+            public static bool operator <(Tardis t1, Tardis t2)
+            {
+                if (t1 is null)
+                {
+                    return t2 != null;
+                }
+                if (t2 is null)
+                {
+                    return false;
+                }
+                if (t1.whichDrWho == 10)
+                {
+                    return false;
+                }
+                if (t2.whichDrWho == 10)
+                {
+                    return true;
+                }
+                return t1.whichDrWho < t2.whichDrWho;
+            }
+            public static bool operator >(Tardis t1, Tardis t2)
+            {
+                return t2 < t1; 
+            }
+
+            public static bool operator <=(Tardis t1, Tardis t2)
+            {
+                return t2 < t1 || t1 == t2;
+            }
+            public static bool operator >=(Tardis t1, Tardis t2)
+            {
+                return t2 > t1 || t1 == t2;
+            }
+
+        }
+
+
+    }
+}
+
+
---
+++ UnitTest2#9-10/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using static UnitTest2_9_10.Program;
+using System.Xml.Linq;
+
+namespace UnitTest2_9_10
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Footballer footballer = new Footballer
+            {
+                name = "Francis Bacon",
+                position = "Foward",
+                age = 22,
+                team = "Barcelona"
+
+            };
+
+            NationalTeam natTeam = new NationalTeam
+            {
+                natname = "Spain"
+            };
+
+            Tickets ticket = new Tickets
+            {
+                ticketprice = 19.99
+            };
+
+            MyMethod(footballer);
+            MyMethod(natTeam);
+            MyMethod(ticket);
+        }
+        
+        static void MyMethod(object obj)
+        {
+            if (obj is ITeam team)
+            {
+                team.getPlayers();
+            }
+
+            if (obj is IStadium stadium)
+            {
+                stadium.getTickets();
+            }
+        }
+
+        public interface ITeam
+        {
+            void getPlayers();
+
+
+        }
+        public interface IStadium
+        {
+            void getTickets();
+        }
+
+        public abstract class Player
+        {
+            public string name;
+            public string position;
+            public int age;
+
+            public virtual void play()
+            {
+
+            }
+
+
+            public abstract void transfer();
+        }
+        public class Footballer : Player, ITeam
+        {
+            public string team;
+
+
+            public override void transfer()
+            {
+
+            }
+
+            public void getPlayers()
+            {
+            }
+        }
+
+        public class Tickets : IStadium
+        {
+            public double ticketprice; 
+
+            public void getTickets()
+            {
+            }
+        }
+
+        public class NationalTeam : ITeam
+        {
+            public string natname;
+
+            public void getPlayers()
+            {
+            }
+        }
+
+
+
+
+    }    
+}
