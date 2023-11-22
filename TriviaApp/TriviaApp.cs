24b13499563a5c931c1f97087439942de8a7247f
Sam Lynch <s2003lynch@gmail.com>
2023-11-21 23:33:05 -0500

    triviaapp

---
+++ TriviaApp/TriviaApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace TriviaApp
+{
+    class TriviaResult
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string corrent_answer;
+        public List<string> incorrect_answers;
+    }
+
+    class Trivia
+    {
+        public int response_code;
+        public List<TriviaResult> triviaResults;
+    }
+
+   internal class ProgramMain
+    {
+        public static void Main(string[] args)
+        {
+            string url = null;
+            string s = null;
+
+            HttpWebRequest request;
+            HttpWebResponse response;
+            StreamReader reader;
+
+            url = "https://opentdb.com/api.php?amount=1&type=multiple";
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+            response = (HttpWebResponse)request.GetResponse();
+            reader = new StreamReader(response.GetResponseStream());
+            s = reader.ReadToEnd();
+            reader.Close();
+
+            Trivia trivia = JsonConvert.DeserializeObject<TriviaResult>(s);
+
+            trivia.triviaResults[0].question = HttpUtility.HtmlDecode(trivia.triviaResults[0].question);
+        }
+    }
+        
+}
